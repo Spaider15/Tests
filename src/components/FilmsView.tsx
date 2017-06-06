33d0c87f3ba675656f35e59786ce71553ecf105e
ia.busarov <ia.busarov@tensor.ru>
2017-06-06 17:33:08 +0300

    add hash route

--- src/components/FilmsView.tsx
+++ src/components/FilmsView.tsx
@@ -12,7 +12,7 @@ interface IProps {
 export default (props: IProps) => {
     const data = props.data;
     const filmsList = data.films.map((film: IFilm, key: number) =>
-        <li key={key}><Link to={`/film/${film.id}`}>{film.name}</Link></li>);
+        <li key={key}><Link to={`film/${film.id}`}>{film.name}</Link></li>);
     return (
         <Route exact path="/" key="main" render={() => (<ul>{filmsList}</ul>)}/>
     );
