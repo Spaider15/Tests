94a128aed253d0f48e36031f7bf26a3723f9aabf
ia.busarov <ia.busarov@tensor.ru>
2017-06-06 16:23:48 +0300

    fix routes

--- src/containers/Films.tsx
+++ src/containers/Films.tsx
@@ -5,10 +5,9 @@
     Route,
 } from "react-router-dom";
  import FilmsView from "../components/FilmsView";
- import FilmItem from "./FilmItem";
  import { queryToAPI } from "../helpers";
  import { IFilmsState } from "../types";
-
+ import FilmItem from "./FilmItem";
 
  export default class Films extends React.Component<{}, IFilmsState> {
     constructor(props: any) {
@@ -20,18 +19,17 @@
 
     public render() {
         if (this.state.loading) {
-            return (<Route exact path="/" render={() => (<h1>Loading...</h1>)}/>);
+            return (<Route exact path="/Tests/" render={() => (<h1>Loading...</h1>)}/>);
         }
         if (this.state.data) {
-
             return (
                     <div>
                         <FilmsView data={this.state.data} />
-                        <Route path="/film/:filmId" key="film" component={FilmItem}/>
+                        <Route path="/Tests/film/:filmId" key="film" component={FilmItem}/>
                     </div>
             );
         } else {
-            return (<Route exact path="/" render={() => (<h1>data is empty</h1>)}/>);
+            return (<Route exact path="/Tests/" render={() => (<h1>data is empty</h1>)}/>);
         }
     }
     public async componentDidMount() {
